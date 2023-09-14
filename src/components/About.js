d4c3aa657a7bd9fb3b574208cf3d78b7b133aa73
92hoge1bif <92hoge1bif@hft-stuttgart.de>
2023-09-14 16:14:04 +0200

    Added: Pages, Routing, fix  react router dom

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>Über uns</h2>
+      <p>Eine kurze Beschreibung Ihres Unternehmens hier einfügen.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h2>Kontaktieren Sie uns</h2>
+      <p>Kontaktinformationen hier einfügen.</p>
+    </div>
+  );
+}
+
+export default Contact;
