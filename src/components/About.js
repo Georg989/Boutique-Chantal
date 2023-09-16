accd6a9fb5e419be7a72a8b087e5e67d40f5fb09
92hoge1bif <92hoge1bif@hft-stuttgart.de>
2023-09-16 15:30:11 +0200

    added a few things

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './style/styles.css';
 
 function About() {
   return (
-    <div>
+    <div className="about">
       <h2>Über uns</h2>
-      <p>Test test</p>
+      <p>Willkommen bei Boutique Chantal, Ihrem Modegeschäft für exklusive Bekleidung und Accessoires. Wir sind stolz darauf, unseren Kunden hochwertige Produkte und erstklassigen Service anzubieten.</p>
+      <p>Unsere Leidenschaft für Mode treibt uns an, immer die neuesten Trends zu verfolgen und Ihnen die besten Auswahlmöglichkeiten zu bieten. Unser erfahrenes Team steht Ihnen gerne zur Verfügung, um Ihnen bei der Auswahl von Produkten zu helfen, die perfekt zu Ihrem Stil passen.</p>
+      <p>Vielen Dank, dass Sie uns vertrauen. Wir freuen uns darauf, Sie in unserem Geschäft begrüßen zu dürfen.</p>
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import './style/styles.css';
 
 function Contact() {
   return (
-    <div>
+    <div className="contact">
       <h2>Kontaktieren Sie uns</h2>
-      <p>Kontaktinformationen hier einfügen.</p>
+      <p>Wir freuen uns darauf, von Ihnen zu hören. Kontaktieren Sie uns über die folgenden Informationen:</p>
+      <ul>
+        <li><strong>Adresse:</strong> Musterstraße 123, 12345 Musterstadt</li>
+        <li><strong>Telefon:</strong> +49 123 456789</li>
+        <li><strong>E-Mail:</strong> kontakt@boutiquechantal.com</li>
+      </ul>
+      <p>Sie können auch das folgende Formular verwenden, um uns eine Nachricht zu senden:</p>
+      <form>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" required />
+
+        <label htmlFor="email">E-Mail:</label>
+        <input type="email" id="email" name="email" required />
+
+        <label htmlFor="message">Nachricht:</label>
+        <textarea id="message" name="message" rows="4" required></textarea>
+
+        <button type="submit">Nachricht senden</button>
+      </form>
     </div>
   );
 }
