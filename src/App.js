fe75843aff76562282e6d16a4bd5e591e22c2b86
92hoge1bif <92hoge1bif@hft-stuttgart.de>
2023-09-20 00:07:29 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
-import Contact from './components/Contact';
-import About from './components/About';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={LandingPage} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/about" component={About} />
       </Switch>
     </Router>
   );
