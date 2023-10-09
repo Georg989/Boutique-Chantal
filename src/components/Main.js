c7e15b2fabdecc427cdb8267a31358e94ffe200f
Georg989 <georgij.hofmann@gmail.com>
2023-10-09 16:53:55 +0200

    fixed text allignment

--- src/components/Main.js
+++ src/components/Main.js
@@ -113,19 +113,19 @@ Auch Männer können bei uns modische Anzüge finden.
               <section>
                   <div className="tab-row">
                       <div className="col-4">
-                          <h2>Columns</h2>
+                          <h2>Abendkleider</h2>
                           <p>
                               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga porro sapiente, corporis dignissimos laudantium illo expedita corrupti asperiores esse quae.
                           </p>
                       </div>
                       <div className="col-4">
-                          <h2>Columns</h2>
+                          <h2>Accessoires</h2>
                           <p>
                               Repellat qui, dolores nemo asperiores incidunt quidem dolorum. Quos, deleniti neque architecto dolore magni aliquid. Inventore suscipit consequuntur excepturi ipsam.
                           </p>
                       </div>
                       <div className="col-4">
-                          <h2>Columns</h2>
+                          <h2>Schuhe</h2>
                           <p>
                               Doloribus perferendis a ipsum ad, consectetur animi aliquam maxime, earum aspernatur, ea commodi tempore suscipit explicabo cupiditate at nemo quae.
                           </p>
